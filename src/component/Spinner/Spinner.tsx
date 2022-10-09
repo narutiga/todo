e090fd1bf9e86f3620816b2103fec18da6454888
narutiga <100295602+narutiga@users.noreply.github.com>
2022-10-10 08:31:04 +0900

    ✨ スピナー追加

---
+++ src/component/Spinner/Spinner.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+
+/** @package */
+export const Spinner: FC = () => {
+  return (
+    <div className="my-5 mx-auto h-8 w-8 animate-spin rounded-full border-2 border-rose-400 border-t-transparent"></div>
+  );
+};
