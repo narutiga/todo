f2ace4c31145e969548c1ab78e433ae9e8f3fed9
narutiga <100295602+narutiga@users.noreply.github.com>
2022-10-10 08:32:27 +0900

    ✨ 認証機能追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,43 @@
 import "src/styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import type { CustomAppPage } from "next/app";
 import { MantineProvider } from "@mantine/core";
+import { QueriesObserver, QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { supabase } from "src/lib/util/supabase";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const App: CustomAppPage = ({ Component, pageProps }) => {
+  const { push, pathname } = useRouter();
+  const validateSession = async () => {
+    const user = supabase.auth.user();
+    if (user && pathname === "/") {
+      push("/dashboard");
+    } else if (!user && pathname !== "/") {
+      await push("/");
+    }
+  };
+  supabase.auth.onAuthStateChange((event, _) => {
+    if (event === "SIGNED_IN" && pathname === "/") {
+      push("/dashboard");
+    }
+    if (event === "SIGNED_OUT") {
+      push("/");
+    }
+  });
+  useEffect(() => {
+    validateSession();
+  });
+
   const getLayout =
     Component.getLayout ||
     ((page) => {
@@ -10,15 +45,18 @@ const App: CustomAppPage = ({ Component, pageProps }) => {
     });
 
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: "light",
-      }}
-    >
-      {getLayout(<Component {...pageProps} />)}
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "light",
+        }}
+      >
+        {getLayout(<Component {...pageProps} />)}
+      </MantineProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
--- src/pages_component/index/page.tsx
+++ src/pages_component/index/page.tsx
@@ -1,10 +1,70 @@
-import Link from "next/link";
+import { useState, FormEvent } from "react";
+import type { NextPage } from "next";
+import { useMutateAuth } from "src/lib/hook/useMutateAuth";
+import { IconCrown } from "@tabler/icons";
 
 /** @package */
-export const Auth = () => {
+export const Auth: NextPage = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    loginMutation,
+    registerMutation,
+  } = useMutateAuth();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isLogin) {
+      loginMutation.mutate();
+    } else {
+      registerMutation.mutate();
+    }
+  };
+
   return (
     <div>
-      <Link href="./dashboard">一覧</Link>
+      <IconCrown className="h-12 w-12 text-orange-400" />
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            required
+            className="my-2 rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-500 focus:border-orange-400 focus:outline-none"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            required
+            className="my-2 rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-500 focus:border-orange-400 focus:outline-none"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        <button
+          type="submit"
+          className="mr-auto ml-auto group relative flex justify-center rounded-full bg-orange-300 py-2 px-4 text-sm font-medium text-gray-600 hover:bg-orange-200"
+        >
+          {isLogin ? "Sing In" : "Sing Up"}
+        </button>
+        <button
+          onClick={() => setIsLogin(!isLogin)}
+          className="cursor-pointer font-medium border-none outline-none bg-transparent text-gray-300 hover:text-orange-300 underline"
+        >
+          change mode ?
+        </button>
+      </form>
     </div>
   );
 };
